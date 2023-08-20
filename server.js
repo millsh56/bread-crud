8b3ce7f881e7058ce72fdba47a572a37c8d9e5b3
Hannah Mills <millsh56@gmail.com>
2023-08-20 14:20:24 -0400

    adding unique title to each page

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ require('dotenv').config()
 const PORT = process.env.port
 console.log(PORT)
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an AWESOME App about Breads!')
